9d61028b4dce838e8b9271f29a3ad620a1568b9a
AdanSmith <smith.paredes.13@gmail.com>
2023-03-21 00:15:23 -0300

    Ejercicio 2.3 completado

--- tp1_1.c
+++ tp1_1.c
@@ -9,8 +9,8 @@ int main()
     int a, *p;
     a = 3;
     p = &a;
-
-    printf("\nContenido del puntero: %d\n", *p);
+    
+    printf("\nContenido del puntero: %p\n", p);
     printf("Direcci%cn de memoria almacenada por el puntero: %p\n", 162, p);
     printf("Direcci%cn de memoria de la variable: %p\n", 162, &a);
     printf("Direcci%cn de memoria del puntero: %p\n", 162, &p);
